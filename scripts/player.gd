1753c189e90d7552e4f8db5368206b8866281bb3
connortodd21 <connortodd21@gmail.com>
2026-01-29 14:37:59 -0800

    Add basic character control and animations

---
+++ scripts/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+@onready var animations := $Animations
+@onready var jump_sound = $JumpSound
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -850.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add animation
+	if velocity.x > 1 or velocity.x < -1:
+		animations.animation = "run"
+	else:
+		animations.animation = "idle"
+	
+	# Add the gravity and use jumping animatino
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		animations.animation = "jump"
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+	# ensure player faces correct direction
+	if direction == 1:
+		animations.flip_h = false
+	elif direction == -1:
+		animations.flip_h = true
